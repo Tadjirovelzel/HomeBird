95bc6ac2f5d7c4cfb779855b1a5c057686179fe9
lduynkerke <lduynkerke@gmail.com>
2023-04-06 12:44:36 +0200

    Add function c_read_file
    Function to read file from SD card or SPIFFS to a buffer in order to be able to send the data over mqtt

--- test_video/src/main.cpp
+++ test_video/src/main.cpp
@@ -11,7 +11,25 @@ void setup() {
 
 void loop(){
   video::record("/sdcard/new_file.avi");
-  //video::copy("/sdcard/new_file.avi", "/spiffs/new_file.avi");
+  //video::copy("/sdcard/new_file.avi", "/sdcard/new_file2.avi");
+
+  // Read file to buffer in order to be able to send over mqtt
+  int err;
+  size_t f_size;
+  char *f_data;
+
+  f_data = video::c_read_file("/sdcard/new_file.avi", &err, &f_size);
+
+  if (err) {
+    Serial.printf("Error copying file: err%d \n",err);
+    free(f_data);
+  }
+  else {
+    Serial.println("File written to buffer");
+    Serial.printf("Size = %d \n",f_size);
+    free(f_data);
+  }
+
   video::clear("/sdcard/new_file.avi");
   delay(5000);
 }
